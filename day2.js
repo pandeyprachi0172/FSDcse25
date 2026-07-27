6616e8c7ad28ff98d6d40fdc5affbfd2cca1f0e5
pandeyprachi0172 <pandeyprachi0172@gmail.com>
2026-07-27 09:50:13 +0530

    js

---
+++ day2.js
@@ -0,0 +1,34 @@
+// // callback function :means function as a parameter
+// function hello (n1,n2,callback){
+//     console.log("Hello World");
+//     callback();
+//     cb();
+// }
+// let a=10;
+// let b=20;
+// console.log(hello(a+b,sayHi));
+// console.log(hello(a,b,sayHello()));
+// console.log(hello(a,b,function demo (){
+//      console.log("callback is calling")
+// }));
+// function sayHi(){
+//     console.log("callback function");
+
+// }
+// sayHi();
+// function sayHello(){
+//      console.log("this ia second callback function")
+// }
+
+// sayHello();
+
+
+
+
+
+                    //  PRomosies in javascript  
+        const promiseOne=new Promise(( resolve,reject)=>{
+
+
+        })
+        promiseOne.then((result)).catch()
---
+++ script.js
@@ -0,0 +1,36 @@
+// // //function in javascript
+// // function hello (){
+// //     console.log("this is function");
+
+// // }
+// // hello;
+// //     console .log("syncrounous javascript");
+
+// // asyncronous java script
+// // arrow function
+// // variable :var,let and const
+// // syntax: ()=>{}
+//     const hello = ()=>{
+//         console.log ( async function );
+//         setTimeout(()=>{
+//             console.log("A");
+//         })
+
+        
+            
+        
+    
+//     },2000
+
+//     hello();
+//     console.log("B")
+// function as parameter argument 
+function  hello(n1,n2){
+    console.log  (n1+n2);
+    console.log(" argument")
+
+}
+let a=10;
+let b=20;
+hello(a,b);
+
