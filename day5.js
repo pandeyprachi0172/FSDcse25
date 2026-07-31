679fa2b82da4f250736cb4373648dfa221f7acb4
pandeyprachi0172 <pandeyprachi0172@gmail.com>
2026-07-31 11:58:50 +0530

    day5

---
+++ day5.js
@@ -0,0 +1,6 @@
+//EVENT
+// USe event emmitter is class in which we have to use emit (event  emit par)
+// progarm 1 and 2
+// emit ()on ()
+const EventEmitter=require("events");
+const event=new EventEmitter();
