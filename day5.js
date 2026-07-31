90560e74eca05da7386ca64ac97aea1ccbd855d0
pandeyprachi0172 <pandeyprachi0172@gmail.com>
2026-07-31 12:19:48 +0530

    changes

--- day5.js
+++ day5.js
@@ -1,6 +1,45 @@
-//EVENT
-// USe event emmitter is class in which we have to use emit (event  emit par)
-// progarm 1 and 2
-// emit ()on ()
-const EventEmitter=require("events");
-const event=new EventEmitter();
+// EVENT
+// Use EventEmitter class in which we have to use emit() and on()
+// Program 1 and 2
+// emit() - to trigger an event
+// on() - to listen for an event
+// once() - event will trigger only one time
+
+// const EventEmitter = require("events");
+
+// const event = new EventEmitter();
+
+// event.on("greet", () => {
+//     console.log("This is event emitter");
+// });
+
+// event.once("greet", () => {
+//     console.log("Event triggered only one time");
+// });
+
+// event.emit("greet");
+// event.emit("greet");
+// event.emit("greet");
+// event.emit("greet");
+// event.emit("greet");
+// event.emit("greet");
+
+// Program 1: Create custom EventEmitter that triggers "greet" or "exit"
+
+const EventEmitter = require("events");
+
+const event = new EventEmitter();
+
+// Create greet event
+event.on("greet", () => {
+    console.log("Hello! This is greet event");
+});
+
+// Create exit event
+event.on("exit", () => {
+    console.log("Program is exiting...");
+});
+
+// Trigger events
+event.emit("greet");
+event.emit("exit");
