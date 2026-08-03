16872ab8c53a919ba34b47103784ac974a86f448
pandeyprachi0172 <pandeyprachi0172@gmail.com>
2026-08-03 14:36:25 +0530

    writefile()

--- day6.js
+++ day6.js
@@ -1,1 +1,9 @@
-///create a onclick evrnt 
+// File module : fs module in node js
+//CRUD operations: Create, Read, Update, Delete
+// CReate a file: writefile() and readfile()
+const fs=require('fs');
+fs.writeFile('myfile.txt','',(err)=>{
+    if(err) throw err;
+    console.log('File created successfully');
+
+})
