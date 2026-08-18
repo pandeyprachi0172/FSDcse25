a4d9937fe30751bcf5b2b3883ac0194040c22539
pandeyprachi0172 <pandeyprachi0172@gmail.com>
2026-08-18 16:19:46 +0530

    PORT HOST

--- HTTP Module/day8.js
+++ HTTP Module/day8.js
@@ -2,7 +2,11 @@ import http from "http"; // Import the built-in HTTP module
 
 // Create the HTTP server
 const server = http.createServer((req, res) => {
-    // Send a response to the client
+    res.writeHead(200, { "Content-Type": "text/html" });
+
+    res.write("<h1>This is my own server</h1>");
+
+    // Send normal text to the browser
     res.write("Hello Prachi! Server is working.");
 
     // End the response
@@ -13,3 +17,18 @@ const server = http.createServer((req, res) => {
 server.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
+
+
+
+
+
+import http from "http"; // Import the built-in HTTP module
+
+// Create the HTTP server
+const server = http.createServer((req, res) => {
+    res.setHeader("content-type","text/html");
+    res.end('response from server end');
+});
+server.listen(POST,HOST,()=>{
+    const PORT=process.env.PORT
+})
