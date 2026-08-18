61aab0a847e32594338e476f82b14e317725ef6d
pandeyprachi0172 <pandeyprachi0172@gmail.com>
2026-08-18 16:00:56 +0530

    req,res

--- HTTP Module/day8.js
+++ HTTP Module/day8.js
@@ -1,11 +1,15 @@
-// Create your own server using HTTP
-import http from "http";
+import http from "http"; // Import the built-in HTTP module
 
+// Create the HTTP server
 const server = http.createServer((req, res) => {
+    // Send a response to the client
     res.write("Hello Prachi! Server is working.");
+
+    // End the response
     res.end();
 });
 
+// Start the server on port 3000
 server.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
