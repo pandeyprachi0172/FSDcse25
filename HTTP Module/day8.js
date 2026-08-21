5d3f3f774f29575c508d9d3091b71154cff50579
pandeyprachi0172 <pandeyprachi0172@gmail.com>
2026-08-21 11:52:39 +0530

    program3

--- HTTP Module/day8.js
+++ HTTP Module/day8.js
@@ -1,39 +1,70 @@
-import http from "http"; // Import the built-in HTTP module
+// import http from "http"; // Import the built-in HTTP module
 
-// Create the HTTP server
+// // Create the HTTP server
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, { "Content-Type": "text/html" });
+
+//     res.write("<h1>This is my own server</h1>");
+
+//     // Send normal text to the browser
+//     res.write("Hello Prachi! Server is working.");
+
+//     // End the response
+//     res.end();
+// });
+
+// // Start the server on port 3000
+// server.listen(3000, () => {
+//     console.log("Server is running on port 3000");
+// });
+
+
+
+
+
+// import http from "http"; // Import the built-in HTTP module
+
+// // Create the HTTP server
+// const server = http.createServer((req, res) => {
+//     res.setHeader("content-type","text/html");
+//     res.end('response from server end');
+// });
+// server.listen(POST,HOST,()=>{
+//     const PORT=process.env.PORT
+// })
+
+
+
+//  <--------------------program 3------------------->
+// Create a basic HTTP server  using .createSErver().Respond with "Hello World" and return headers +status code
+const http = require('http');
+
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/html" });
+  if (req.url === '/success') {
+    res.writeHead(200, {
+      'Content-Type': 'text/plain',
+      'X-Custom-Header': 'Hello'
+    });
 
-    res.write("<h1>This is my own server</h1>");
+    res.end('Hello World');
+  } 
+  else if (req.url === '/forbidden') {
+    res.writeHead(403, {
+      'Content-Type': 'text/plain',
+      'X-Custom-Header': 'Hello'
+    });
 
-    // Send normal text to the browser
-    res.write("Hello Prachi! Server is working.");
+    res.end('Forbidden');
+  } 
+  else {
+    res.writeHead(404, {
+      'Content-Type': 'text/plain'
+    });
 
-    // End the response
-    res.end();
+    res.end('Not Found');
+  }
 });
 
-// Start the server on port 3000
 server.listen(3000, () => {
-    console.log("Server is running on port 3000");
+  console.log('Server running at http://localhost:3000');
 });
-
-
-
-
-
-import http from "http"; // Import the built-in HTTP module
-
-// Create the HTTP server
-const server = http.createServer((req, res) => {
-    res.setHeader("content-type","text/html");
-    res.end('response from server end');
-});
-server.listen(POST,HOST,()=>{
-    const PORT=process.env.PORT
-})
-
-
-
-
-.addListener.
---
+++ HTTP Module/day9.js
@@ -0,0 +1,9 @@
+//  status code for network 200 :success, 404: page not found ,403:forbidden ,
+// Practice routing using http module in  node.js
+import http from "http";
+const server  =http .createserver  ((req,res )=>{
+
+})
+server.listen (3000,()=>{
+     console.log("server is runnig on http:/localhost: ")
+})
