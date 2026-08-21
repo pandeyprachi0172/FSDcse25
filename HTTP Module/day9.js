075f8e489091ce147b457ccd0686ddd44fd1d01d
pandeyprachi0172 <pandeyprachi0172@gmail.com>
2026-08-21 12:12:50 +0530

    day9.js

--- HTTP Module/day9.js
+++ HTTP Module/day9.js
@@ -1,9 +1,29 @@
 //  status code for network 200 :success, 404: page not found ,403:forbidden ,
-// Practice routing using http module in  node.js
-import http from "http";
-const server  =http .createserver  ((req,res )=>{
+// Practice routing using http module in node.js
 
-})
-server.listen (3000,()=>{
-     console.log("server is runnig on http:/localhost: ")
-})
+import http from "http";
+
+const server = http.createServer((req, res) => {
+
+    if (req.url === "/") {
+        res.statusCode = 200;
+        res.end(" This is Home Page");
+    }
+    else if (req.url === "/about") {
+        res.statusCode = 200;
+        res.end(" This is About Page");
+    }
+    else if (req.url === "/admin") {
+        res.statusCode = 403;
+        res.end("Forbidden");
+    }
+    else {
+        res.statusCode = 404;
+        res.end("Page Not Found");
+    }
+
+});
+
+server.listen(3000, () => {
+    console.log("server is running on http://localhost:3000");
+});
