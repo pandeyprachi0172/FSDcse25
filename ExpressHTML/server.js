1a3d9f681053acb8a2f2cf2c29428bdc061bc2e2
pandeyprachi0172 <pandeyprachi0172@gmail.com>
2026-09-11 12:17:15 +0530

    Assignment 1

---
+++ ExpressHTML/server.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import fs from 'fs';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    fs.readFile('./index.html', 'utf8', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Error reading index.html');
+            return;
+        }
+
+        res.send(data);
+    });
+});
+
+app.get('/about', (req, res) => {
+    fs.readFile('./about.html', 'utf8', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Error reading about.html');
+            return;
+        }
+
+        res.send(data);
+    });
+});
+
+app.get('/contact', (req, res) => {
+    fs.readFile('./contact.html', 'utf8', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Error reading contact.html');
+            return;
+        }
+
+        res.send(data);
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
